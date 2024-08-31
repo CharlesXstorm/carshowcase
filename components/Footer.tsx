89d09c172f7e4c90e17d8c8547dbcb96c6012e30
Charles Ukadike <charlesukadike44@gmail.com>
2024-08-31 07:52:36 +0100

    update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,8 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import { footerLinks } from "@/constants";
+
 const Footer = () => {
   return (
     <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
@@ -14,7 +16,35 @@ const Footer = () => {
             height={18}
             className="object-contain"
           />
-          <p className=""></p>
+          <p className="text-base text-gray-700">
+            Carhub 2023 <br />
+            All rights reserved &copy;
+          </p>
+        </div>
+      </div>
+
+      <div className="footer__links">
+        {footerLinks.map((link) => (
+          <div key={link.title} className="footer__link">
+            <h3 className="font-bold">{link.title}</h3>
+            {link.links.map((item) => (
+              <Link key={item.title} href={item.url} className="text-gray-500">
+                {item.title}
+              </Link>
+            ))}
+          </div>
+        ))}
+      </div>
+
+      <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 py-10 ">
+        <p>@2023 CarHub. All Rights Reserved</p>
+        <div className="footer__copyrights-link">
+          <Link href="/" className="text-gray-500">
+            Privacy Policy
+          </Link>
+          <Link href="/" className="text-gray-500">
+            Terms of Use
+          </Link>
         </div>
       </div>
     </footer>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import CustomButton from "./CustomButton";
 
 const Hero = () => {
-    const handleScroll = ()=>{}
+  const handleScroll = () => {};
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
@@ -18,18 +18,17 @@ const Hero = () => {
         </p>
 
         <CustomButton
-        title="Explore Cars"
-        containerStyles="bg-primary-blue text-white rounded-full mt-10"
-        handleClick={handleScroll}
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
         />
       </div>
 
       <div className="hero__image-container">
         <div className="hero__image">
-            <Image src="/hero.png" alt="hero" fill className="object-contain" />
-            <div className="hero__image-overlay" />
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
         </div>
-
+        <div className="hero__image-overlay" />
       </div>
     </div>
   );
